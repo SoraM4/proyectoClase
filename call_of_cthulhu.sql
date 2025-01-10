6ddbdc7c0994bae901da5bcc43567c68070380d3
SoraM4 <mariodelatorregarcia@gmail.com>
2025-01-10 13:39:58 +0000

    fix: bd

--- call_of_cthulhu.sql
+++ call_of_cthulhu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-01-2025 a las 14:51:20
+-- Tiempo de generación: 10-01-2025 a las 14:24:13
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -70,11 +70,8 @@ CREATE TABLE `characters` (
 --
 
 INSERT INTO `characters` (`ID`, `character_data`) VALUES
-(117659, '{\"STR\":50,\"CON\":40,\"SIZ\":75,\"DEX\":45,\"APP\":65,\"INT\":85,\"Luck\":50,\"EDU\":85,\"POW\":55,\"Age\":20,\"MOV\":7,\"Occupation\":\"ANTIQUARIAN\",\"skills\":{\"Accounting\":5,\"Acting\":5,\"Animal Handling\":5,\"Anthropology\":1,\"Appraise\":5,\"Archaeology\":1,\"Art and Craft\":5,\"Artillery\":1,\"Astronomy\":1,\"Axe\":15,\"Biology\":1,\"Botany\":1,\"Bow\":15,\"Brawl\":25,\"Chainsaw\":10,\"Charm\":15,\"Chemistry\":1,\"Climb\":20,\"Computer Use\":5,\"Credit Rating\":0,\"Cryptography\":1,\"Cthulhu Mythos\":0,\"Demolitions\":1,\"Disguise\":5,\"Diving\":1,\"Dodge\":0,\"Drive Auto\":20,\"Electrical Repair\":10,\"Electronics\":1,\"Engineering\":1,\"Fast Talk\":5,\"Fighting\":0,\"Fine Art\":5,\"Firearms\":0,\"First Aid\":30,\"Flail\":10,\"Flamethrower\":10,\"Forensics\":1,\"Forgery\":5,\"Garrote\":15,\"Geology\":1,\"Handgun\":20,\"Heavy Weapons\":10,\"History\":5,\"Hypnosis\":1,\"Intimidate\":15,\"Jump\":20,\"Language (Other)\":1,\"Language (Own)\":0,\"Law\":5,\"Library Use\":20,\"Listen\":20,\"Locksmith\":1,\"Lore\":1,\"Machine Gun\":10,\"Mathematics\":10,\"Mechanical Repair\":10,\"Medicine\":1,\"Meteorology\":1,\"Natural World\":10,\"Navigate\":10,\"Occult\":5,\"Operate Heavy Machinery\":1,\"Persuade\":10,\"Pharmacy\":1,\"Photography\":5,\"Physics\":1,\"Pilot\":1,\"Psychoanalysis\":1,\"Psychology\":10,\"Read Lips\":1,\"Ride\":5,\"Rifle\":25,\"Science\":1,\"Shotgun\":25,\"Sleight of Hand\":10,\"Spear\":20,\"Spot Hidden\":25,\"Stealth\":20,\"Submachine Gun\":15,\"Survival\":10,\"Sword\":20,\"Swim\":20,\"Throw\":20,\"Track\":10,\"Whip\":5,\"Zoology\":1},\"ID\":117659}'),
-(659969, '{\"STR\":65,\"CON\":35,\"SIZ\":60,\"DEX\":80,\"APP\":60,\"INT\":80,\"POW\":60,\"EDU\":62,\"Luck\":40,\"Age\":20,\"MOV\":8,\"Occupation\":\"ANTIQUARIAN\",\"skills\":{\"Accounting\":5,\"Acting\":5,\"Animal Handling\":5,\"Anthropology\":1,\"Appraise\":5,\"Archaeology\":1,\"Art and Craft\":5,\"Artillery\":1,\"Astronomy\":1,\"Axe\":15,\"Biology\":1,\"Botany\":1,\"Bow\":15,\"Brawl\":25,\"Chainsaw\":10,\"Charm\":15,\"Chemistry\":1,\"Climb\":20,\"Computer Use\":5,\"Credit Rating\":0,\"Cryptography\":1,\"Cthulhu Mythos\":0,\"Demolitions\":1,\"Disguise\":5,\"Diving\":1,\"Dodge\":0,\"Drive Auto\":20,\"Electrical Repair\":10,\"Electronics\":1,\"Engineering\":1,\"Fast Talk\":5,\"Fighting\":0,\"Fine Art\":5,\"Firearms\":0,\"First Aid\":30,\"Flail\":10,\"Flamethrower\":10,\"Forensics\":1,\"Forgery\":5,\"Garrote\":15,\"Geology\":1,\"Handgun\":20,\"Heavy Weapons\":10,\"History\":5,\"Hypnosis\":1,\"Intimidate\":15,\"Jump\":20,\"Language (Other)\":1,\"Language (Own)\":0,\"Law\":5,\"Library Use\":20,\"Listen\":20,\"Locksmith\":1,\"Lore\":1,\"Machine Gun\":10,\"Mathematics\":10,\"Mechanical Repair\":10,\"Medicine\":1,\"Meteorology\":1,\"Natural World\":10,\"Navigate\":10,\"Occult\":5,\"Operate Heavy Machinery\":1,\"Persuade\":10,\"Pharmacy\":1,\"Photography\":5,\"Physics\":1,\"Pilot\":1,\"Psychoanalysis\":1,\"Psychology\":10,\"Read Lips\":1,\"Ride\":5,\"Rifle\":25,\"Science\":1,\"Shotgun\":25,\"Sleight of Hand\":10,\"Spear\":20,\"Spot Hidden\":25,\"Stealth\":20,\"Submachine Gun\":15,\"Survival\":10,\"Sword\":20,\"Swim\":20,\"Throw\":20,\"Track\":10,\"Whip\":5,\"Zoology\":1}}'),
-(722359, '{\"STR\":75,\"CON\":35,\"SIZ\":40,\"DEX\":30,\"APP\":40,\"INT\":50,\"POW\":65,\"EDU\":56,\"Luck\":30,\"Age\":23,\"MOV\":8,\"Occupation\":\"ANTIQUARIAN\",\"skills\":{\"Actuaci\\u00f3n\":5,\"Ametralladora\":47,\"Antropolog\\u00eda\":1,\"Arco\":15,\"Armas de Fuego\":0,\"Armas Pesadas\":10,\"Arqueolog\\u00eda\":1,\"Arte Fino\":5,\"Arte\\/Artesan\\u00eda\":5,\"Artiller\\u00eda\":1,\"Astronom\\u00eda\":1,\"Biolog\\u00eda\":1,\"Bot\\u00e1nica\":1,\"Buceo\":1,\"Buscar\":25,\"Cerrajer\\u00eda\":1,\"Charlataner\\u00eda\":5,\"Ciencia\":1,\"Combate\":0,\"Conducir Autom\\u00f3vil\":20,\"Conocimiento\":1,\"Contabilidad\":5,\"Criptograf\\u00eda\":1,\"Demoliciones\":1,\"Derecho\":5,\"Disfraz\":5,\"Electricidad\":10,\"Electr\\u00f3nica\":1,\"Encanto\":15,\"Escalar\":20,\"Escopeta\":25,\"Escuchar\":20,\"Espada\":20,\"Esquivar\":15,\"Farmacolog\\u00eda\":1,\"Falsificaci\\u00f3n\":5,\"Fisica\":1,\"Fotograf\\u00eda\":5,\"Garrote\":15,\"Geolog\\u00eda\":1,\"Hacha\":15,\"Hipnosis\":1,\"Historia\":5,\"Idioma (Otro)\":1,\"Idioma (Propio)\":56,\"Inform\\u00e1tica\":5,\"Ingenier\\u00eda\":1,\"Intimidar\":15,\"Juego de Manos\":10,\"Lanza\":20,\"Lanzallamas\":10,\"Lanzar\":20,\"Leer Labios\":1,\"L\\u00e1tigo\":5,\"Matem\\u00e1ticas\":10,\"Mayal\":10,\"Mec\\u00e1nica\":10,\"Medicina\":1,\"Medicina Forense\":1,\"Meteorolog\\u00eda\":1,\"Mitos de Cthulhu\":0,\"Montar\":5,\"Motosierra\":10,\"Nadar\":20,\"Naturaleza\":10,\"Ocultismo\":5,\"Operar Maquinaria Pesada\":1,\"Orientarse\":10,\"Persuadir\":10,\"Pilotar\":1,\"Pistola\":20,\"Posesi\\u00f3n de Cr\\u00e9ditos\":0,\"Primeros Auxilios\":30,\"Psicoan\\u00e1lisis\":1,\"Psicolog\\u00eda\":10,\"Qu\\u00edmica\":1,\"Rastrear\":10,\"Rifle\":25,\"Saltar\":20,\"Sigilo\":20,\"Subfusil\":15,\"Supervivencia\":10,\"Tasaci\\u00f3n\":5,\"Uso de Biblioteca\":20,\"Zoolog\\u00eda\":1},\"ID\":722359}'),
-(734526, '{\"STR\":25,\"CON\":65,\"SIZ\":55,\"DEX\":90,\"APP\":65,\"INT\":55,\"POW\":55,\"EDU\":67,\"Luck\":50,\"Age\":20,\"MOV\":8,\"Occupation\":\"ATHLETE\",\"skills\":{\"Accounting\":5,\"Acting\":5,\"Animal Handling\":5,\"Anthropology\":1,\"Appraise\":5,\"Archaeology\":1,\"Art and Craft\":5,\"Artillery\":1,\"Astronomy\":1,\"Axe\":15,\"Biology\":1,\"Botany\":1,\"Bow\":15,\"Brawl\":25,\"Chainsaw\":10,\"Charm\":15,\"Chemistry\":1,\"Climb\":20,\"Computer Use\":5,\"Credit Rating\":0,\"Cryptography\":1,\"Cthulhu Mythos\":0,\"Demolitions\":1,\"Disguise\":5,\"Diving\":1,\"Dodge\":0,\"Drive Auto\":20,\"Electrical Repair\":10,\"Electronics\":1,\"Engineering\":1,\"Fast Talk\":5,\"Fighting\":0,\"Fine Art\":5,\"Firearms\":0,\"First Aid\":30,\"Flail\":10,\"Flamethrower\":10,\"Forensics\":1,\"Forgery\":5,\"Garrote\":15,\"Geology\":1,\"Handgun\":20,\"Heavy Weapons\":10,\"History\":5,\"Hypnosis\":1,\"Intimidate\":15,\"Jump\":20,\"Language (Other)\":1,\"Language (Own)\":0,\"Law\":5,\"Library Use\":20,\"Listen\":20,\"Locksmith\":1,\"Lore\":1,\"Machine Gun\":10,\"Mathematics\":10,\"Mechanical Repair\":10,\"Medicine\":1,\"Meteorology\":1,\"Natural World\":10,\"Navigate\":10,\"Occult\":5,\"Operate Heavy Machinery\":1,\"Persuade\":10,\"Pharmacy\":1,\"Photography\":5,\"Physics\":1,\"Pilot\":1,\"Psychoanalysis\":1,\"Psychology\":10,\"Read Lips\":1,\"Ride\":5,\"Rifle\":25,\"Science\":1,\"Shotgun\":25,\"Sleight of Hand\":10,\"Spear\":20,\"Spot Hidden\":25,\"Stealth\":20,\"Submachine Gun\":15,\"Survival\":10,\"Sword\":20,\"Swim\":20,\"Throw\":20,\"Track\":10,\"Whip\":5,\"Zoology\":1},\"ID\":734526}'),
-(838683, '{\"STR\":55,\"CON\":45,\"SIZ\":80,\"DEX\":25,\"APP\":45,\"INT\":75,\"POW\":70,\"EDU\":49,\"Luck\":40,\"Age\":20,\"MOV\":7,\"Occupation\":\"ARTIST\",\"skills\":{\"Accounting\":5,\"Acting\":5,\"Animal Handling\":5,\"Anthropology\":1,\"Appraise\":5,\"Archaeology\":1,\"Art and Craft\":5,\"Artillery\":1,\"Astronomy\":1,\"Axe\":15,\"Biology\":1,\"Botany\":1,\"Bow\":15,\"Brawl\":25,\"Chainsaw\":10,\"Charm\":15,\"Chemistry\":1,\"Climb\":20,\"Computer Use\":5,\"Credit Rating\":0,\"Cryptography\":1,\"Cthulhu Mythos\":0,\"Demolitions\":1,\"Disguise\":5,\"Diving\":1,\"Dodge\":0,\"Drive Auto\":20,\"Electrical Repair\":10,\"Electronics\":1,\"Engineering\":1,\"Fast Talk\":5,\"Fighting\":0,\"Fine Art\":5,\"Firearms\":0,\"First Aid\":30,\"Flail\":10,\"Flamethrower\":10,\"Forensics\":1,\"Forgery\":5,\"Garrote\":15,\"Geology\":1,\"Handgun\":20,\"Heavy Weapons\":10,\"History\":5,\"Hypnosis\":1,\"Intimidate\":15,\"Jump\":20,\"Language (Other)\":1,\"Language (Own)\":0,\"Law\":5,\"Library Use\":20,\"Listen\":20,\"Locksmith\":1,\"Lore\":1,\"Machine Gun\":10,\"Mathematics\":10,\"Mechanical Repair\":10,\"Medicine\":1,\"Meteorology\":1,\"Natural World\":10,\"Navigate\":10,\"Occult\":5,\"Operate Heavy Machinery\":1,\"Persuade\":10,\"Pharmacy\":1,\"Photography\":5,\"Physics\":1,\"Pilot\":1,\"Psychoanalysis\":1,\"Psychology\":10,\"Read Lips\":1,\"Ride\":5,\"Rifle\":25,\"Science\":1,\"Shotgun\":25,\"Sleight of Hand\":10,\"Spear\":20,\"Spot Hidden\":25,\"Stealth\":20,\"Submachine Gun\":15,\"Survival\":10,\"Sword\":20,\"Swim\":20,\"Throw\":20,\"Track\":10,\"Whip\":5,\"Zoology\":1},\"ID\":838683}');
+(483190, '{\"STR\":45,\"CON\":50,\"SIZ\":65,\"DEX\":85,\"APP\":50,\"INT\":45,\"POW\":55,\"EDU\":70,\"Luck\":35,\"Age\":20,\"MOV\":8,\"Occupation\":\"ANTIQUARIAN\",\"skills\":{\"Actuaci\\u00f3n\":5,\"Ametralladora\":10,\"Antropolog\\u00eda\":1,\"Arco\":15,\"Armas de Fuego\":0,\"Armas Pesadas\":10,\"Arqueolog\\u00eda\":1,\"Arte Fino\":5,\"Arte\\/Artesan\\u00eda\":5,\"Artiller\\u00eda\":1,\"Astronom\\u00eda\":1,\"Biolog\\u00eda\":1,\"Bot\\u00e1nica\":1,\"Buceo\":1,\"Buscar\":25,\"Cerrajer\\u00eda\":1,\"Charlataner\\u00eda\":5,\"Ciencia\":1,\"Combate\":0,\"Conducir Autom\\u00f3vil\":20,\"Conocimiento\":1,\"Contabilidad\":5,\"Cr\\u00e9dito\":30,\"Criptograf\\u00eda\":1,\"Demoliciones\":1,\"Derecho\":5,\"Disfraz\":5,\"Electricidad\":10,\"Electr\\u00f3nica\":1,\"Encanto\":15,\"Escalar\":20,\"Escopeta\":25,\"Escuchar\":20,\"Espada\":43,\"Esquivar\":0,\"Farmacolog\\u00eda\":1,\"Falsificaci\\u00f3n\":5,\"Fisica\":1,\"Fotograf\\u00eda\":5,\"Garrote\":15,\"Geolog\\u00eda\":1,\"Hacha\":15,\"Hipnosis\":1,\"Historia\":5,\"Idioma (Otro)\":70,\"Idioma (Propio)\":0,\"Inform\\u00e1tica\":5,\"Ingenier\\u00eda\":1,\"Intimidar\":15,\"Juego de Manos\":10,\"Lanza\":20,\"Lanzallamas\":10,\"Lanzar\":20,\"Leer Labios\":1,\"L\\u00e1tigo\":5,\"Matem\\u00e1ticas\":10,\"Mayal\":10,\"Mec\\u00e1nica\":10,\"Medicina\":1,\"Medicina Forense\":1,\"Meteorolog\\u00eda\":1,\"Mitos de Cthulhu\":0,\"Montar\":5,\"Motosierra\":10,\"Nadar\":20,\"Naturaleza\":10,\"Ocultismo\":5,\"Operar Maquinaria Pesada\":1,\"Orientarse\":10,\"Persuadir\":10,\"Pilotar\":1,\"Pistola\":20,\"Posesi\\u00f3n de Cr\\u00e9ditos\":0,\"Primeros Auxilios\":30,\"Psicoan\\u00e1lisis\":1,\"Psicolog\\u00eda\":10,\"Qu\\u00edmica\":1,\"Rastrear\":10,\"Rifle\":25,\"Saltar\":20,\"Sigilo\":20,\"Subfusil\":15,\"Supervivencia\":10,\"Tasaci\\u00f3n\":5,\"Uso de Biblioteca\":20,\"Zoolog\\u00eda\":1},\"ID\":483190}'),
+(722359, '{\"STR\":75,\"CON\":35,\"SIZ\":40,\"DEX\":30,\"APP\":40,\"INT\":50,\"POW\":65,\"EDU\":56,\"Luck\":30,\"Age\":23,\"MOV\":8,\"Occupation\":\"ANTIQUARIAN\",\"skills\":{\"Actuaci\\u00f3n\":5,\"Ametralladora\":47,\"Antropolog\\u00eda\":1,\"Arco\":15,\"Armas de Fuego\":0,\"Armas Pesadas\":10,\"Arqueolog\\u00eda\":1,\"Arte Fino\":5,\"Arte\\/Artesan\\u00eda\":5,\"Artiller\\u00eda\":1,\"Astronom\\u00eda\":1,\"Biolog\\u00eda\":1,\"Bot\\u00e1nica\":1,\"Buceo\":1,\"Buscar\":25,\"Cerrajer\\u00eda\":1,\"Charlataner\\u00eda\":5,\"Ciencia\":1,\"Combate\":0,\"Conducir Autom\\u00f3vil\":20,\"Conocimiento\":1,\"Contabilidad\":5,\"Criptograf\\u00eda\":1,\"Demoliciones\":1,\"Derecho\":5,\"Disfraz\":5,\"Electricidad\":10,\"Electr\\u00f3nica\":1,\"Encanto\":15,\"Escalar\":20,\"Escopeta\":25,\"Escuchar\":20,\"Espada\":20,\"Esquivar\":15,\"Farmacolog\\u00eda\":1,\"Falsificaci\\u00f3n\":5,\"Fisica\":1,\"Fotograf\\u00eda\":5,\"Garrote\":15,\"Geolog\\u00eda\":1,\"Hacha\":15,\"Hipnosis\":1,\"Historia\":5,\"Idioma (Otro)\":1,\"Idioma (Propio)\":56,\"Inform\\u00e1tica\":5,\"Ingenier\\u00eda\":1,\"Intimidar\":15,\"Juego de Manos\":10,\"Lanza\":20,\"Lanzallamas\":10,\"Lanzar\":20,\"Leer Labios\":1,\"L\\u00e1tigo\":5,\"Matem\\u00e1ticas\":10,\"Mayal\":10,\"Mec\\u00e1nica\":10,\"Medicina\":1,\"Medicina Forense\":1,\"Meteorolog\\u00eda\":1,\"Mitos de Cthulhu\":0,\"Montar\":5,\"Motosierra\":10,\"Nadar\":20,\"Naturaleza\":10,\"Ocultismo\":5,\"Operar Maquinaria Pesada\":1,\"Orientarse\":10,\"Persuadir\":10,\"Pilotar\":1,\"Pistola\":20,\"Posesi\\u00f3n de Cr\\u00e9ditos\":0,\"Primeros Auxilios\":30,\"Psicoan\\u00e1lisis\":1,\"Psicolog\\u00eda\":10,\"Qu\\u00edmica\":1,\"Rastrear\":10,\"Rifle\":25,\"Saltar\":20,\"Sigilo\":20,\"Subfusil\":15,\"Supervivencia\":10,\"Tasaci\\u00f3n\":5,\"Uso de Biblioteca\":20,\"Zoolog\\u00eda\":1},\"ID\":722359}');
 
 -- --------------------------------------------------------
 
@@ -144,69 +141,70 @@ INSERT INTO `skills` (`id`, `name`, `base_percentage`, `is_modern`, `is_uncommon
 (20, 'Conducir Automóvil', 20, 0, 0, 0, NULL),
 (21, 'Conocimiento', 1, 0, 1, 1, NULL),
 (22, 'Contabilidad', 5, 0, 0, 0, NULL),
-(23, 'Criptografía', 1, 0, 0, 1, 'Ciencia'),
-(24, 'Demoliciones', 1, 0, 1, 0, NULL),
-(25, 'Derecho', 5, 0, 0, 0, NULL),
-(26, 'Disfraz', 5, 0, 0, 0, NULL),
-(27, 'Electricidad', 10, 0, 0, 0, NULL),
-(28, 'Electrónica', 1, 1, 0, 0, NULL),
-(29, 'Encanto', 15, 0, 0, 0, NULL),
-(30, 'Escalar', 20, 0, 0, 0, NULL),
-(31, 'Escopeta', 25, 0, 0, 1, 'Armas de Fuego'),
-(32, 'Escuchar', 20, 0, 0, 0, NULL),
-(33, 'Espada', 20, 0, 0, 1, 'Combate'),
-(34, 'Esquivar', 0, 0, 0, 0, NULL),
-(35, 'Farmacología', 1, 0, 0, 1, 'Ciencia'),
-(36, 'Falsificación', 5, 0, 0, 1, 'Arte/Artesanía'),
-(37, 'Fisica', 1, 0, 0, 1, 'Ciencia'),
-(38, 'Fotografía', 5, 0, 0, 1, 'Arte/Artesanía'),
-(39, 'Garrote', 15, 0, 0, 1, 'Combate'),
-(40, 'Geología', 1, 0, 0, 1, 'Ciencia'),
-(41, 'Hacha', 15, 0, 0, 1, 'Combate'),
-(42, 'Hipnosis', 1, 0, 1, 0, NULL),
-(43, 'Historia', 5, 0, 0, 0, NULL),
-(44, 'Idioma (Otro)', 1, 0, 0, 1, NULL),
-(45, 'Idioma (Propio)', 0, 0, 0, 0, NULL),
-(46, 'Informática', 5, 1, 0, 0, NULL),
-(47, 'Ingeniería', 1, 0, 0, 1, 'Ciencia'),
-(48, 'Intimidar', 15, 0, 0, 0, NULL),
-(49, 'Juego de Manos', 10, 0, 0, 0, NULL),
-(50, 'Lanza', 20, 0, 0, 1, 'Combate'),
-(51, 'Lanzallamas', 10, 0, 0, 1, 'Armas de Fuego'),
-(52, 'Lanzar', 20, 0, 0, 0, NULL),
-(53, 'Leer Labios', 1, 0, 1, 0, NULL),
-(54, 'Látigo', 5, 0, 0, 1, 'Combate'),
-(55, 'Matemáticas', 10, 0, 0, 1, 'Ciencia'),
-(56, 'Mayal', 10, 0, 0, 1, 'Combate'),
-(57, 'Mecánica', 10, 0, 0, 0, NULL),
-(58, 'Medicina', 1, 0, 0, 0, NULL),
-(59, 'Medicina Forense', 1, 0, 0, 1, 'Ciencia'),
-(60, 'Meteorología', 1, 0, 0, 1, 'Ciencia'),
-(61, 'Mitos de Cthulhu', 0, 0, 0, 0, NULL),
-(62, 'Montar', 5, 0, 0, 0, NULL),
-(63, 'Motosierra', 10, 0, 0, 1, 'Combate'),
-(64, 'Nadar', 20, 0, 0, 0, NULL),
-(65, 'Naturaleza', 10, 0, 0, 0, NULL),
-(66, 'Ocultismo', 5, 0, 0, 0, NULL),
-(67, 'Operar Maquinaria Pesada', 1, 0, 0, 0, NULL),
-(68, 'Orientarse', 10, 0, 0, 0, NULL),
-(69, 'Persuadir', 10, 0, 0, 0, NULL),
-(70, 'Pilotar', 1, 0, 0, 1, NULL),
-(71, 'Pistola', 20, 0, 0, 1, 'Armas de Fuego'),
-(72, 'Posesión de Créditos', 0, 0, 0, 0, NULL),
-(73, 'Primeros Auxilios', 30, 0, 0, 0, NULL),
-(74, 'Psicoanálisis', 1, 0, 0, 0, NULL),
-(75, 'Psicología', 10, 0, 0, 0, NULL),
-(76, 'Química', 1, 0, 0, 1, 'Ciencia'),
-(77, 'Rastrear', 10, 0, 0, 0, NULL),
-(78, 'Rifle', 25, 0, 0, 1, 'Armas de Fuego'),
-(79, 'Saltar', 20, 0, 0, 0, NULL),
-(80, 'Sigilo', 20, 0, 0, 0, NULL),
-(81, 'Subfusil', 15, 0, 0, 1, 'Armas de Fuego'),
-(82, 'Supervivencia', 10, 0, 0, 1, NULL),
-(83, 'Tasación', 5, 0, 0, 0, NULL),
-(84, 'Uso de Biblioteca', 20, 0, 0, 0, NULL),
-(85, 'Zoología', 1, 0, 0, 1, 'Ciencia');
+(23, 'Crédito', 0, 0, 0, 0, NULL),
+(24, 'Criptografía', 1, 0, 0, 1, 'Ciencia'),
+(25, 'Demoliciones', 1, 0, 1, 0, NULL),
+(26, 'Derecho', 5, 0, 0, 0, NULL),
+(27, 'Disfraz', 5, 0, 0, 0, NULL),
+(28, 'Electricidad', 10, 0, 0, 0, NULL),
+(29, 'Electrónica', 1, 1, 0, 0, NULL),
+(30, 'Encanto', 15, 0, 0, 0, NULL),
+(31, 'Escalar', 20, 0, 0, 0, NULL),
+(32, 'Escopeta', 25, 0, 0, 1, 'Armas de Fuego'),
+(33, 'Escuchar', 20, 0, 0, 0, NULL),
+(34, 'Espada', 20, 0, 0, 1, 'Combate'),
+(35, 'Esquivar', 0, 0, 0, 0, NULL),
+(36, 'Farmacología', 1, 0, 0, 1, 'Ciencia'),
+(37, 'Falsificación', 5, 0, 0, 1, 'Arte/Artesanía'),
+(38, 'Fisica', 1, 0, 0, 1, 'Ciencia'),
+(39, 'Fotografía', 5, 0, 0, 1, 'Arte/Artesanía'),
+(40, 'Garrote', 15, 0, 0, 1, 'Combate'),
+(41, 'Geología', 1, 0, 0, 1, 'Ciencia'),
+(42, 'Hacha', 15, 0, 0, 1, 'Combate'),
+(43, 'Hipnosis', 1, 0, 1, 0, NULL),
+(44, 'Historia', 5, 0, 0, 0, NULL),
+(45, 'Idioma (Otro)', 1, 0, 0, 1, NULL),
+(46, 'Idioma (Propio)', 0, 0, 0, 0, NULL),
+(47, 'Informática', 5, 1, 0, 0, NULL),
+(48, 'Ingeniería', 1, 0, 0, 1, 'Ciencia'),
+(49, 'Intimidar', 15, 0, 0, 0, NULL),
+(50, 'Juego de Manos', 10, 0, 0, 0, NULL),
+(51, 'Lanza', 20, 0, 0, 1, 'Combate'),
+(52, 'Lanzallamas', 10, 0, 0, 1, 'Armas de Fuego'),
+(53, 'Lanzar', 20, 0, 0, 0, NULL),
+(54, 'Leer Labios', 1, 0, 1, 0, NULL),
+(55, 'Látigo', 5, 0, 0, 1, 'Combate'),
+(56, 'Matemáticas', 10, 0, 0, 1, 'Ciencia'),
+(57, 'Mayal', 10, 0, 0, 1, 'Combate'),
+(58, 'Mecánica', 10, 0, 0, 0, NULL),
+(59, 'Medicina', 1, 0, 0, 0, NULL),
+(60, 'Medicina Forense', 1, 0, 0, 1, 'Ciencia'),
+(61, 'Meteorología', 1, 0, 0, 1, 'Ciencia'),
+(62, 'Mitos de Cthulhu', 0, 0, 0, 0, NULL),
+(63, 'Montar', 5, 0, 0, 0, NULL),
+(64, 'Motosierra', 10, 0, 0, 1, 'Combate'),
+(65, 'Nadar', 20, 0, 0, 0, NULL),
+(66, 'Naturaleza', 10, 0, 0, 0, NULL),
+(67, 'Ocultismo', 5, 0, 0, 0, NULL),
+(68, 'Operar Maquinaria Pesada', 1, 0, 0, 0, NULL),
+(69, 'Orientarse', 10, 0, 0, 0, NULL),
+(70, 'Persuadir', 10, 0, 0, 0, NULL),
+(71, 'Pilotar', 1, 0, 0, 1, NULL),
+(72, 'Pistola', 20, 0, 0, 1, 'Armas de Fuego'),
+(73, 'Posesión de Créditos', 0, 0, 0, 0, NULL),
+(74, 'Primeros Auxilios', 30, 0, 0, 0, NULL),
+(75, 'Psicoanálisis', 1, 0, 0, 0, NULL),
+(76, 'Psicología', 10, 0, 0, 0, NULL),
+(77, 'Química', 1, 0, 0, 1, 'Ciencia'),
+(78, 'Rastrear', 10, 0, 0, 0, NULL),
+(79, 'Rifle', 25, 0, 0, 1, 'Armas de Fuego'),
+(80, 'Saltar', 20, 0, 0, 0, NULL),
+(81, 'Sigilo', 20, 0, 0, 0, NULL),
+(82, 'Subfusil', 15, 0, 0, 1, 'Armas de Fuego'),
+(83, 'Supervivencia', 10, 0, 0, 1, NULL),
+(84, 'Tasación', 5, 0, 0, 0, NULL),
+(85, 'Uso de Biblioteca', 20, 0, 0, 0, NULL),
+(86, 'Zoología', 1, 0, 0, 1, 'Ciencia');
 
 --
 -- Índices para tablas volcadas
@@ -250,7 +248,7 @@ ALTER TABLE `biography`
 -- AUTO_INCREMENT de la tabla `characters`
 --
 ALTER TABLE `characters`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=838684;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=881065;
 
 --
 -- AUTO_INCREMENT de la tabla `occupations`
